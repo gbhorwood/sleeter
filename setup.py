df070b40a238feed6834a231ada9c360aae82af8
ghorwood <ghorwood@cloverhitch.ca>
2019-05-28 09:27:34 -0600

    Better setup.py

--- setup.py
+++ setup.py
@@ -4,33 +4,60 @@ import re
 import os
 from setuptools import setup
 
+
+###
+# configurations
+#
+project = "sleeter"
+author = "grant horwood"
+author_email = "ghorwood@cloverhitch.ca"
+project_url = "https://github.com/gbhorwood/sleeter"
+description = ""
+python_requires = ">=3.7"
+
+
 ###
 # get version number using regex from the file
 #
 version = re.search(
     '^__version__\s*=\s*"(.*)"',
-    open('sleeter/sleeter.py').read(),
+    open(project+'/'+project+'.py').read(),
     re.M
     ).group(1)
+
+
+###
+# Get the contents of README for the long description
+#
+with open("README.rst", "rb") as f:
+    long_description = f.read().decode("utf-8")
+
 
 ###
 # get install_requires data from requirements.txt
 #
-requirements_txt_path = str(os.path.dirname(os.path.realpath(__file__)))+"/requirements.txt"
+requirements_txt_path = str(os.path.dirname(os.path.realpath(__file__)))+'/'+project+'/requirements.txt'
 install_requires = []
 if os.path.isfile(requirements_txt_path):
     with open(requirements_txt_path) as f:
         install_requires = f.read().splitlines()
 
 
-setup (
-    name = "sleeter",
-    packages = ["sleeter"],
-    entry_points = {
-        "console_scripts": ['sleeter = sleeter.sleeter:main']
+###
+# run setup()
+#
+setup(
+    name=project,
+    packages=[project],
+    entry_points={
+        "console_scripts": [project+' = '+project+'.'+project+':main']
         },
-    python_requires=">=3.7",
-    install_requires = install_requires,
-    author = "grant horwood",
-    url = "http://cloverhitch.ca"
+    version=version,
+    python_requires=python_requires,
+    install_requires=install_requires,
+    author=author,
+    author_email=author_email,
+    description=description,
+    long_description=long_description,
+    url=project_url
 )
